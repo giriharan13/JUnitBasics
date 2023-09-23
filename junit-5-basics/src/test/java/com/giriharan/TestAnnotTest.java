07e06342cf05e15180be5718077137905f2a6984
giriharan13 <lionking0304channel@gmail.com>
2023-09-24 00:04:38 +0530

    about @Test annotation

---
+++ junit-5-basics/src/test/java/com/giriharan/TestAnnotTest.java
@@ -0,0 +1,20 @@
+package com.giriharan;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestAnnotTest {
+
+	@Test                   // @Test annotation is used to tell JUnit that this is a test 
+	void test() {
+		System.out.println("This test ran!");  // this test passes
+	}
+	
+	@Test
+	void test2()
+	{
+		fail("This test failed! :(");   // this test fails 
+	}
+
+}
