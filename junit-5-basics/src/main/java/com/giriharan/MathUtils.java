51d1d226e7aa3970d56ada04ec6eb90784b358fc
giriharan13 <lionking0304channel@gmail.com>
2023-09-24 10:27:30 +0530

    adding testAddArray(),addArray() and some information about Assertions

--- junit-5-basics/src/main/java/com/giriharan/MathUtils.java
+++ junit-5-basics/src/main/java/com/giriharan/MathUtils.java
@@ -7,6 +7,22 @@ public class MathUtils {
 		return a+b;
 	}
 	
+	public int[] addArray(int[] a,int[] b)
+	{
+		int max = (a.length>b.length)?a.length:b.length;
+		int res[] = new int[max];
+		for(int i=0;i<max;i++)
+		{
+			if(i<a.length && i<b.length)
+				res[i] = a[i]+b[i];
+			else if(i<a.length)
+				res[i] = a[i];
+			else
+				res[i] = b[i];
+		}
+		return res;
+	}
+	
 	public int subtract(int a,int b)
 	{
 		return a-b;
--- junit-5-basics/src/test/java/com/giriharan/MathUtilsTest.java
+++ junit-5-basics/src/test/java/com/giriharan/MathUtilsTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 class MathUtilsTest {
 
 	@Test
-	void test() {
+	void testAdd() {
 		MathUtils mu = new MathUtils();
 		int expected = 2;            // Expected value
 		int actual = mu.add(1, 1);   // Actual value
@@ -23,9 +23,33 @@ class MathUtilsTest {
 		 *      void assertTrue(boolean condition)
 		 *		void assertFalse(boolean condition)
 		 *		void assertNull(Object obj)
-		 *		void assertNotNull(Object obj)      
+		 *		void assertNotNull(Object obj)   
+		 *      .............   
 		 */
 		
 	}
+	
+	@Test
+	void testAddArray() {
+		MathUtils mu = new MathUtils();
+		int a[] = {1,2,4};
+		int b[] = {4,5,6};
+		int[] actual = mu.addArray(a,b);
+		int expected[] = {5,7,9};
+		
+		//assertArrayEquals(expected,actual);
+		
+		// you can also add a message as a third argument. This helps if multiple devs are working on the code
+		assertArrayEquals(expected,actual,"the addArray() method is supposed to add two arrays");
+	}
+	
+	/*
+	 * there are also methods for compare arrays and iterables
+	 *     void assertArrayEquals(int[] a,int[] b)
+	 *     void	assertIterableEquals(Iterable<?> expected, Iterable<?> actual)
+	 *     ............
+	 */
+	
+	// visit the junit doc for all the assert methods
 
 }
